46c9315e21dc09ec23ec30d2573bd220c276f3bd
jjamesliu <liujameszhi@gmail.com>
2025-07-16 13:02:59 -0700

    added youtube video functionality

--- src/app/components/YoutubePostCard.tsx
+++ src/app/components/YoutubePostCard.tsx
@@ -15,19 +15,23 @@ export default function YoutubePostCard() {
     useEffect(() => {
         fetch('api/youtube')
         .then(res => res.json())
-        .then(data => setVideos(data))
+        .then(data => {
+            console.log(data)
+            setVideos(data)
+    })
     }, []);
 
     return (
-        <div>
+        <div className='group relative bg-gray-800/60 hover:bg-gray-800/80 border border-gray-700/50 hover:border-gray-600/70 rounded-xl p-6 transition-all duration-300 hover:shadow-xl hover:shadow-blue-500/10 hover:scale-[1.02]'>
             {videos.map(video => (
                 <a key={video.id}
                 href={video.link}
                 target="_blank"
                 rel="noopener noreferrer">
                     <img src={video.thumbnail}
-                    alt={video.title}></img>
-                    <h3 className='text-white font-semibiold'>{video.title}</h3>
+                    alt={video.title}
+                    className='w-full rounded-xl'></img>
+                    <h3 className='mt-4 font-bold'>{video.title}</h3>
                 </a>
             ))}
         </div>
